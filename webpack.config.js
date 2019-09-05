b6f8a323c18adf3c9061750c44c51257b7a415b0
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-09-05 16:00:16 +0200

    Add optimize-js

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,63 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
 
 module.exports = env => {
   const envValue = env || 'production';
 
+  let optimizationObject = {
+    minimize: false
+  };
+
+  let pluginsArray = [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ];
+
   if (envValue === 'production') {
-    return {
-      mode: envValue,
-      entry: './src/index.js',
-      output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            loader: 'babel-loader'
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              'style-loader',
-              // Translates CSS into CommonJS
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true
-                }
-              },
-              // Compiles Sass to CSS
-              'sass-loader'
-            ]
-          }
-        ]
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: 'src/index.html',
-          filename: 'index.html',
-          inject: 'body'
-        })
-      ]
-    };
-  } else if (envValue === 'development') {
-    return {
-      mode: envValue,
-      entry: './src/index.js',
-      optimization: {
-        minimize: false
-      },
-      output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            loader: 'babel-loader'
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              'style-loader',
-              // Translates CSS into CommonJS
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true
-                }
-              },
-              // Compiles Sass to CSS
-              'sass-loader'
-            ]
-          }
-        ]
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: 'src/index.html',
-          filename: 'index.html',
-          inject: 'body'
-        })
-      ]
-    };
+    pluginsArray = [
+      ...pluginsArray,
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    ];
   }
+
+  return {
+    mode: envValue,
+    entry: './src/index.js',
+    optimization: optimizationObject,
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            },
+            // Compiles Sass to CSS
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: pluginsArray
+  };
 };
