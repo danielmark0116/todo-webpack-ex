4ec3f35caa69709f0eade9690cabd0486ec99e3d
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-09-05 15:37:14 +0200

    Add env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,76 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
+module.exports = env => {
+  const envValue = env || 'production';
+
+  if (envValue === 'production') {
+    return {
+      mode: envValue,
+      entry: './src/index.js',
+      output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
+      module: {
+        rules: [
           {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
+            test: /\.js$/,
+            loader: 'babel-loader'
           },
-          // Compiles Sass to CSS
-          'sass-loader'
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+              },
+              // Compiles Sass to CSS
+              'sass-loader'
+            ]
+          }
         ]
       }
-    ]
+    };
+  } else if (envValue === 'development') {
+    return {
+      mode: envValue,
+      entry: './src/index.js',
+      optimization: {
+        minimize: false
+      },
+      output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+              },
+              // Compiles Sass to CSS
+              'sass-loader'
+            ]
+          }
+        ]
+      }
+    };
   }
 };
